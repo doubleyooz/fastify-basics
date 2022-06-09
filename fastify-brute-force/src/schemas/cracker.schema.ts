10e2e0cd209c06eb833acf9eb1d955579bf7caf2
Waifu Forever <itachiut1r4@gmail.com>
2022-06-09 18:16:53 -0300

    update: schemas

--- fastify-brute-force/src/schemas/cracker.schema.ts
+++ fastify-brute-force/src/schemas/cracker.schema.ts
@@ -1,3 +1,10 @@
+export interface Request {
+    min: number;
+    max: number;
+    url?: string;
+    username?: string;
+}
+
 const crackPassword = {
     summary: 'Crack Password via brute force',
     consumes: ['application/json'],
@@ -5,9 +12,10 @@ const crackPassword = {
         type: 'object',
         additionalProperties: false,
         properties: {
-            min: { type: 'number' },
-            max: { type: 'number' },
+            min: { type: 'number', minimum: 1, maximum: 15 },
+            max: { type: 'number', minimum: 2, maximum: 20 },
             url: { type: 'string' },
+            username: {type: 'string'}
         },
         required: ['min', 'max'],
     },
