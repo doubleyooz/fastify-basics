b71222a6b902b0893711aebfc2aeed732cf644c3
Waifu Forever <itachiut1r4@gmail.com>
2022-06-09 22:25:21 -0300

    add: user.controller store()

--- fastify-user-auth/src/controllers/user.controller.ts
+++ fastify-user-auth/src/controllers/user.controller.ts
@@ -1,12 +1,38 @@
 import User, { IUser } from '../models/user.model';
+import { hashPassword } from '../utils/password.util';
 
 const store = async (req: any, reply: any) => {
     const { email, password, name }: IUser = req.body;
 
-    if (await User.exists({ email: email }))
-        return reply.code(400).send({ error: 'duplicate key' });
+    const newUser: IUser = new User({
+        email: email,
+        password: await hashPassword(password),
+        name: name
+    });
 
-    return reply.code(200).send({ data: 'okay' });
+    newUser
+        .save()
+        .then(result => {
+            return reply.code(201).send({
+                data: { email: result.email, _id: result._id },
+                message: "User created!",
+            });
+        })
+        .catch(err => {
+            if (err.name === 'MongoServerError' && err.code === 11000) {
+                //There was a duplicate key error
+                return reply.code(400).send({
+                    message: "Email already in use",
+                    err: err,
+                });
+            }
+            return reply.code(400).send({
+                message: "Bad Request",
+                err: err,
+            });
+        });
+    
+    
 };
 
 export default { store };
