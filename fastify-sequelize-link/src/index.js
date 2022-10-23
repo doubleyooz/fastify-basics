ac967dbac300b29cf497ba7a166d91c39b1b0b90
Waifu Forever <itachiut1r4@gmail.com>
2022-10-23 09:59:50 -0300

    chore: database connection     add sequelize connection;     set sequelize config;

--- fastify-sequelize-link/src/index.js
+++ fastify-sequelize-link/src/index.js
@@ -1,10 +1,16 @@
 import { app } from './config/fastify.config.js';
+import { sequelize } from './database/config/database.config.js';
 
 const PORT = process.env.PORT ? process.env.PORT : 5000;
 console.log(process.env.PORT);
+
+
 const start = async () => {
     try {
-        await app.listen({port: PORT});
+        await app.listen({ port: PORT });
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+
     } catch (error) {
         console.log(error);
         app.log.error(error);
