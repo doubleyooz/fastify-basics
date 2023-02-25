948b4c9ea4da9acb5e9c5444769f7cfc325d00b6
Waifu Forever <sirjoandersonwf@gmail.com>
2023-02-25 19:25:26 -0300

    chore: update cookie configuration     add client domain;

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -44,6 +44,7 @@ const signIn = async (req: FastifyRequest, reply: FastifyReply) => {
     req.headers.authorization = `Bearer ${token}`;
 
     reply.setCookie('jid', refreshToken, {
+        domain: `${process.env.CLIENT}`,
         path: '/refresh-token',
         httpOnly: true,
     });
